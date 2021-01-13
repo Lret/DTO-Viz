9452b718abee4c09ac9efe37d90430f9a9d72d2a
Luigi <lret@ziggo.nl>
2021-01-13 20:42:00 +0100

    We got a visual

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import {useRef, useEffect} from 'react';
 import Rete from "rete";
 import ConnectionPlugin from 'rete-connection-plugin';
-import VueRenderPlugin from 'rete-vue-render-plugin';
-import ReactRenderPlugin, { Node, Socket, Control } from 'rete-react-render-plugin';
+// import VueRenderPlugin from 'rete-vue-render-plugin';
+import ReactRenderPlugin/*, { Node, Socket, Control }*/ from 'rete-react-render-plugin';
+// import ContextMenuPlugin from 'rete-context-menu-plugin';
 import './App.css';
 
+const numSocket = new Rete.Socket('Number value');
 
 class NumComponent extends Rete.Component {
   constructor() {
@@ -15,16 +17,16 @@ class NumComponent extends Rete.Component {
   //   super('Number');
   // }
 
-  builder(node:Node) {
+  builder(node) {
     // let out = new Rete.Output('num', 'Number', numSocket);
-    let out = new Rete.Output('num', 'Number', Socket());
+    let out = new Rete.Output('num', 'Number', numSocket);
 
     node.addOutput(out);
 
     return node;
   }
 
-  worker(node:Node, inputs, outputs) {
+  worker(node, inputs, outputs) {
     outputs['num'] = node.data.num;
   }
 }
@@ -41,8 +43,10 @@ function App() {
     if (!container) throw new Error("#rete element was not found.");
     const editor = new Rete.NodeEditor('demo@0.1.0', container);
   
-    editor.use(ConnectionPlugin)
+    editor.use(ConnectionPlugin);
     editor.use(ReactRenderPlugin);
+    // editor.use(VueRenderPlugin);
+    // editor.use(ContextMenuPlugin);
   
     const numComponent = new NumComponent();
     editor.register(numComponent);  
@@ -51,12 +55,44 @@ function App() {
     // Events
     const engine = new Rete.Engine('demo@0.1.0');
     engine.register(numComponent);
+
+    //
+    // numComponent.createNode().then((num) => {
+    //   num.position = [80, 200];
+    //   editor.addNode(num);
+    // });
+
+    (async () => {
+      const num = await numComponent.createNode();
+      num.position = [80, 200];
+      editor.addNode(num);
+    })();
+
     
+    // const n1 = await components[0].createNode({ num: 2 });
+    // const n2 = await components[0].createNode({ num: 0 });
+    // const add = await components[1].createNode();
+
+    // n1.position = [80, 200];
+    // n2.position = [80, 400];
+    // add.position = [500, 240];
+
+    // editor.addNode(n1);
+    // editor.addNode(n2);
+    // editor.addNode(add);
+
+    // editor.connect(n1.outputs.get('num'), add.inputs.get('num1'));
+    // editor.connect(n2.outputs.get('num'), add.inputs.get('num2'));
+    //
+
     editor.on(["process", "nodecreated", "noderemoved", "connectioncreated", "connectionremoved"], async () => {
         await engine.abort();
         await engine.process(editor.toJSON());
     });
 
+    editor.view.resize();
+    editor.trigger('process');
+    console.log("Rete inialized")
   },[/*Run once*/])
 
   // Render
@@ -64,7 +100,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         {/* <div id="rete"></div> */}
-        <div ref={rete_ref} style={{height:"80%", width:"80%"}} />
+        <div ref={rete_ref} style={{height:"80% !importent", width:"100%"}} />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
