04ede0727867dd7d9d28997a9c88a3518c3e8ef9
Luigi <lret@ziggo.nl>
2021-01-27 21:12:24 +0100

    smaller

--- src/components/graph/controls/num.tsx
+++ src/components/graph/controls/num.tsx
@@ -1,13 +1,68 @@
-import React, { useState } from "react";
+import React, { Dispatch, FunctionComponent, SetStateAction, useEffect, useState } from "react";
 import { Control } from "rete";
 
-const NumComponent = ({inital, handleChange, readonly}) => {
-  const [value, setValue] = useState(inital || 0);
+interface BaseProps<T> {
+  initalValue: T,
+  updateValue: (value : T) => /*T |*/ void,
+  readonly : boolean  
+}
+
+class BaseController<T, P extends BaseProps<T> = BaseProps<T>> extends Control {
+  render: string = 'react';
+  component: /*JSX.Element |*/FunctionComponent<P>;
+  props: P;
+
+  constructor(emitter, key: string, node, template: FunctionComponent<P>, initalValue : T, props = {}, readonly: boolean = false) {
+    // Set parents key
+    super(key);
+
+    // Set template // Could also be set with Generic
+    this.component = template;
+
+    // Set Inital value
+    node.data[key] = node.data[key] ?? initalValue;
+
+    // Set Props
+    this.props = {
+      initalValue: node.data[key],
+      updateValue: (value) => {
+        this.putData(key, value);
+        emitter.trigger("process");
+        //return value;
+      },
+      readonly,
+      ...props
+    } as P;
+  }
+}
+
+function MakeController<T, P extends BaseProps<T> = BaseProps<T>>(inital : T, template : FunctionComponent<P>, props : P) : (emitter, key: string, node, readonly: boolean) => BaseController<T, P> {
+  return (emitter, key: string, node, readonly: boolean = false) => 
+    new BaseController<T, P>(emitter, key, node, template, inital, props, readonly);
+}
+
+
+function useWrapState<S>(initialState: S | (() => S), setEffectState: (value: S) => void): [S, Dispatch<SetStateAction<S>>] {
+  const [value, setValue] = useState(initialState);
+
+  useEffect(() => {
+    setEffectState(value)
+  }, [setEffectState, value]);
+
+  return [value, setValue];
+  // return [value, (value: S) => {
+  //   sideEffectState(value);
+  //   setValue(value);
+  // }]
+};
+
+const NumComponent = ({initalValue, updateValue, readonly}: BaseProps<number>) => {
+  const [value, setValue] = useWrapState(initalValue, updateValue);
   // const ... = useController(emitter, key, node, readonly = false);
 
   return (<input type="number" value={value} readOnly={readonly} onChange={ e => {
       const newValue = +e.target.value;
-      handleChange(newValue);
+      setValue(newValue);
       setValue(newValue);
     }}
   />)
@@ -15,26 +70,8 @@ const NumComponent = ({inital, handleChange, readonly}) => {
   // return controller
 }
 
-export default class NumController extends Control {
-  render: string = 'react';
-  component = NumComponent;
-
-  constructor(emitter, key, node/*name*/, readonly = false) {
-    super(key);
-
-    node.data[key] = node.data[key] ?? 0;
-
-    // @ts-ignore
-    this.props = {  
-      readonly,
-      inital: node.data[key],
-      handleChange: (value) => {
-        this.putData(key, value);
-        emitter.trigger("process");
-      }
-    };
+export default class NumController extends BaseController<number> {
+  constructor(emitter, key: string, node, readonly: boolean = false) {
+    super(emitter, key, node, NumComponent, 0, readonly)
   }
 }
-
-// Create builder
-// Or create hook
